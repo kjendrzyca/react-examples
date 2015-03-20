eeb63448077922d0e8dc7efb37f0510b460cf391
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-03-20 20:57:23 +0100

    Added form manipulation for single entity in crud example.

--- crud/crudSingleEntity.jsx
+++ crud/crudSingleEntity.jsx
@@ -3,21 +3,36 @@
 var React = require('react');
 
 var SingleEntity = React.createClass({
+    onFormSubmit: function(event) {
+        event.preventDefault();
+        var formValues = {
+            id: this.refs.id.getDOMNode().value,
+            name: this.refs.name.getDOMNode().value,
+            description: this.refs.description.getDOMNode().value
+        };
+
+        // TODO: post to server
+        console.log(formValues);
+    },
+
     render: function() {
         return (
             <div className="container">
-                <div className="input-field">
-                    <label htmlFor="id" className="active">Id</label>
-                    <input type="text" id="id" value={ this.props.cartoonCharacter.id } readOnly />
-                </div>
-                <div className="input-field">
-                    <label htmlFor="id" className="active">Name</label>
-                    <input type="text" id="name" value={ this.props.cartoonCharacter.name } />
-                </div>
-                <div className="input-field">
-                    <label htmlFor="id" className="active">Description</label>
-                    <input type="text" id="description" value={ this.props.cartoonCharacter.description } />
-                </div>
+                <form onSubmit={ this.onFormSubmit }>
+                    <div className="input-field">
+                        <label htmlFor="id" className="active">Id</label>
+                        <input type="text" id="id" ref="id" defaultValue={ this.props.cartoonCharacter.id } readOnly />
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor="id" className="active">Name</label>
+                        <input type="text" id="name" ref="name" defaultValue={ this.props.cartoonCharacter.name } />
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor="id" className="active">Description</label>
+                        <input type="text" id="description" ref="description" defaultValue={ this.props.cartoonCharacter.description } />
+                    </div>
+                    <button type="submit" className="btn">Submit</button>
+                </form>
             </div>
         );
     }
