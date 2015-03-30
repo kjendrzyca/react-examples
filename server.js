ea26f1a5010ee42889ef171ce8093b70e38a5b26
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-03-30 22:32:18 +0200

    Static files are read on evey request to simplify development.

--- server.js
+++ server.js
@@ -8,28 +8,22 @@ var portNumber = 8888;
 
 var cartoonCharacters = require('./cartoonCharactersData.js');
 
-var crudHtml = {};
-var crudBundle = {};
-
-fs.readFile('crud/crud.html', function(error, data) {
-    crudHtml = data;
-});
-fs.readFile('crud/crud.bundle.js', function(error, data) {
-    crudBundle = data;
-});
-
 var router = new Router({ showLog: true });
 
 var paths = ['/crud', '/crud/cartoonCharacters/{id}', '/crud/cartoonCharacters/new'];
 
 router.httpGet(paths, function(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.end(crudHtml);
+    fs.readFile('crud/crud.html', function(error, crudHtml) {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end(crudHtml);
+    });
 });
 
 router.httpGet('/crud.bundle.js', function(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/javascript'});
-    response.end(crudBundle);
+    fs.readFile('crud/crud.bundle.js', function(error, crudBundle) {
+        response.writeHead(200, {'Content-Type': 'text/javascript'});
+        response.end(crudBundle);
+    });
 });
 
 // API
