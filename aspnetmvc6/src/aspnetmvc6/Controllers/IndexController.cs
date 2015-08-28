c9e17f3842825acf5bf5190ac664120c2c3529d9
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-08-28 22:20:48 +0200

    added mvc

---
+++ aspnetmvc6/src/aspnetmvc6/Controllers/IndexController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace aspnetmvc6.Controllers
+{
+    [Route("index")]
+    public class IndexController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- aspnetmvc6/src/aspnetmvc6/Startup.cs
+++ aspnetmvc6/src/aspnetmvc6/Startup.cs
@@ -13,20 +13,24 @@ namespace aspnetmvc6
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
-            {
-                if (context.Request.Path == "/relativePath")
-                {
-                    await context.Response.WriteAsync("relativePath");
-                    return;
-                }
+            app.UseMvc();
+            app.UseStaticFiles();
 
-                await context.Response.WriteAsync("Hello World! sup?");
-            });
+            //app.Run(async (context) =>
+            //{
+            //    if (context.Request.Path == "/relativePath")
+            //    {
+            //        await context.Response.WriteAsync("relativePath");
+            //        return;
+            //    }
+
+            //    await context.Response.WriteAsync("Hello World! sup?");
+            //});
         }
     }
 }
