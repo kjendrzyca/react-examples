0f73aa890e3e4ce70abdbfa5a570765e167639f9
Krzysztof Jendrzyca <kjendrzyca@future-processing.com>
2016-01-18 11:40:43 +0100

    removing empty line in gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -32,7 +32,6 @@ module.exports = function(grunt) {
             }
         }
     });
-
     grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.registerTask('default', ['watch']);
